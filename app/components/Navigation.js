2f4af1890173f70b4c07566cb98894559f7ca00a
Himanshu6205 <7himanshu69@gmail.com>
2025-10-30 12:49:47 +0530

    "logo "

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,14 +1,28 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Navbar() {
   return (
-    <nav>
+ <div>
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <Image
+          src="/download.jpeg"   // your logo inside /public folder
+          alt="Logo"
+          width={50}
+          height={50}
+        />
+        <h2 style={{ margin: 0 }}>MyWebsite</h2>
+      </div>
+
+     <nav>
+      
       <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
+
         <li><Link href="/">Home</Link></li>
         <li><Link href="/services">Services</Link></li>
         <li><Link href="/about">About</Link></li>
         <li><Link href="/contact">Contact</Link></li>
       </ul>
     </nav>
+ </div>
   );
 }
